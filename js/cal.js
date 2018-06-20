3b202263f91bf4e8b5da20991fd9a81ca102b4e0
patricia <garcia-patricia@live.com>
2018-06-20 14:28:57 -0400

    added subtract button, and function

--- js/cal.js
+++ js/cal.js
@@ -15,6 +15,8 @@
 	var selPlus = document.querySelector("#plus");
 
 	var selMinus = document.querySelector('#minus');
+
+	var userChoice = "";
 	//console.log(num1);
 	//console.log(num2);
 	//console.log(equals);
@@ -24,20 +26,57 @@
 
 
 	//Functions instructions when trigger is hit
-	function addStuff() {
+	function doStuff() {
+		var total = "";
+		//console.log(userChoice);
 		// console.log("from addStuff");
 		// document.getElementById('input1').value;
 		// console.log(num1.value);
 		// console.log(num2.value);
-		var total = parseInt(num1.value) + parseInt(num2.value);
+		if(userChoice == "add"){
+			total = parseInt(num1.value) + parseInt(num2.value);}
+		else
+		{
+			total = parseInt(num1.value) - parseInt(num2.value);
+		}
 		// console.log(total);
 		outcome.innerHTML = total;
 
 	}
 
+	function selCal(evt){
+		//console.log("From selCal");
+		//condition
+		/*if(selPlus.innerHTML == "+") {
+			console.log("User clicked +");
+		}
+		
+
+		if(selMinus.innerHTML == "-"){
+			console.log("User clicked -");
+		} */
+
+		//look up event argument
+		// can see what the user clicked on by adding 'current target.id'
+		//console.log(evt.currentTarget.id);
+		if(evt.currentTarget.id == "plus"){
+			console.log("add");
+			userChoice = "add";
+
+
+		}else{
+			console.log("sub");
+			userChoice = "sub";
+		}
+
+
+
+	}
 
 	//Listeners
-	equals.addEventListener("click", addStuff, false);
+	selPlus.addEventListener("click", selCal, false);
+	selMinus.addEventListener("click", selCal, false);
+	equals.addEventListener("click", doStuff, false);
 
 
 }) ();
